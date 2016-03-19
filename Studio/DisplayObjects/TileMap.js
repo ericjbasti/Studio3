01b90b81c9011d5ddaa8bcad966762c528bb8214
Eric Basti <ericjbasti@gmail.com>
2016-03-19 17:25:40 -0400

    tile map

--- Studio/DisplayObjects/TileMap.js
+++ Studio/DisplayObjects/TileMap.js
@@ -1,8 +1,8 @@
-Studio.load = function(path, type, callback){
+Studio.load = function(path, type, callback, who){
 	
 	var request = new XMLHttpRequest();
 
-	request.onload = function() {
+	request.onload = function studio_load_onload() {
 		var data = null;
 		if ((request.status >= 200 && request.status < 400) || request.status == 0) {
 			console.log('%cStudio loaded file : '+path, Studio.statStyle);
@@ -11,7 +11,12 @@ Studio.load = function(path, type, callback){
 				data = JSON.parse(request.responseText);
 			}
 			if(callback){
-				callback(data)
+				if(who){
+					console.log(callback, who)
+					callback.call(who,data)
+				}else{
+					callback(data)
+				}
 			}
 		} else {
 			this.onerror();
@@ -31,11 +36,19 @@ Studio.load = function(path, type, callback){
 
 
 
-Studio.TileMap = function(data, set){
-	this.cache = new Studio.Cache(30*32,16*32,1);
+Studio.TileMap = function(width, height, resolution, attr){
+	this.cache = new Studio.Cache(width, height, resolution);
+	this.buffer = new Studio.Cache(width, height, resolution);
+	this.offsetX = 0;
+	this.offsetY = 0;
+	this.data = null
+	if (attr) {
+		this.apply(attr)
+	}
 }
 
 Studio.TileMap.prototype = {
+	apply: Studio.apply,
 	constructor: Studio.TileMap,
 	build: function(data,set,sx,sy,mx,my){
 		var map = data;
@@ -54,30 +67,42 @@ Studio.TileMap.prototype = {
 				buffer.drawImage(set.set.image, _x*set.tileWidth, _y*set.tileHeight, set.tileWidth, set.tileHeight, x*set.tileWidth, y*set.tileHeight, set.tileWidth, set.tileHeight)
 			}
 		}
+		this.buffer.ctx.drawImage(this.cache.image,0,0);
+		document.body.appendChild(this.buffer.image)
 		this.cache.ready = true;
 	},
+	offsetMap : function offsetMap(dx,dy){
+		var dx = dx || 0;
+		var dy = dy || 0;
+		this.offsetX+=dx
+		this.offsetY+=dy
+		this.cache.ctx.clearRect(0,0,this.cache.width,this.cache.height)
+		this.cache.ctx.drawImage(this.buffer.image,dx*this.tileset.tilewidth,dy*this.tileset.tileheight)
+		for(var i in this.data.layers){
+			this.build( this.data.layers[i], this.tileset, this.offsetX, this.offsetY, dx, dy);
+		}
+	},
+	_onLoad : function test(result){
+		if (!result) {
+			console.log("The image isn't ready so we need to wait.");
+			return;
+		}
+		if(result == true){
+			// map  = new Studio.TileMap();
+			console.log( 'The Image is ready, we can now create the tileset from the information provided, and then build the map from the data layer.' );
+			this.offsetMap(0,0)
+		}
+	},
 	onMapLoad : function(data){
+		this.data = data
 		var set = data.tilesets[0];
 		var setimage = new Studio.Image('assets/'+set.image);
-		var tileset =  new Studio.TileSet( setimage, set.tilewidth, set.tileheight, set.imagewidth);
-		var test = function test(result){
-			if (!result) {
-				console.log("The image isn't ready so we need to wait.");
-				return;
-			}
-			if(result == true){
-				// map  = new Studio.TileMap();
-				console.log( 'The Image is ready, we can now create the tileset from the information provided, and then build the map from the data layer.' );
-				for(var i in data.layers){
-					map.build( data.layers[i], tileset);
-				}
-			}
-		}
+		this.tileset =  new Studio.TileSet( setimage, set.tilewidth, set.tileheight, set.imagewidth);
 		// change to addListenerFunction( function ) ... this explains what the variable needs to be.
-		setimage.status.addListener(test);
+		setimage.status.addListener('_onLoad', this);
 	},
 	load : function(asset, type){
-		Studio.load(asset, type, this.onMapLoad)
+		Studio.load(asset, type, this.onMapLoad, this)
 	}
 };
 
