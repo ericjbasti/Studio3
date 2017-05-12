62f9a1f8d83518e1389d03588b8e3352ac308e8c
Eric Basti <ericjbasti@gmail.com>
2017-05-12 19:13:19 -0400

    updated camera code for when its not tracking

--- Studio/DisplayObjects/Camera.js
+++ Studio/DisplayObjects/Camera.js
@@ -6,12 +6,12 @@
 Studio.Camera = function(stage) {
 	this.stage 		= {width: stage.width, height: stage.height}
 	this.tracking 	= null
-	this.focus		= {x:0,y:0}
+	this.focus		= {x: 0, y: 0}
 	this.bound 		= null
 	this.active		= true
-	this.visibleArea = new Studio.Rect({x:0,y:0,width:stage.width,height:stage.height, color: new Studio.Color(200,0,255,.4)})
+	this.visibleArea = new Studio.Rect({x: 0, y: 0, width: stage.width, height: stage.height, color: new Studio.Color(200, 0, 255, .4)})
 	stage.addChild(this.visibleArea);
-	this.matrix 	= 	new Float32Array([1,0,0,0,1,0,0,0,1]);
+	this.matrix 	= 	new Float32Array([1, 0, 0, 0, 1, 0, 0, 0, 1]);
 }
 
 Studio.inherit(Studio.Camera, Studio.DisplayObject)
@@ -22,15 +22,20 @@ Studio.Camera.prototype.updateRect = function() {
 	this.right	= (this.bound._dwidth * this.scaleX) - this.focus.x
 	this.bottom	= (this.bound._dheight * this.scaleY) - this.focus.y
 }
-Studio.Camera.prototype.update_visbile_area = function(){
-	this.visibleArea.x = this.x;
-	this.visibleArea.y = this.y;
-	this.visibleArea.width = (this.stage.width/this.scaleX) -10;
-	this.visibleArea.height = (this.stage.height/this.scaleY) -10;
+Studio.Camera.prototype.update_visbile_area = function() {
+	if (this.tracking) {
+		this.visibleArea.x = this.x
+		this.visibleArea.y = this.y
+	} else {
+		this.visibleArea.x = this.x + stage.width / 2
+		this.visibleArea.y = this.y + stage.height / 2
+	}
+	this.visibleArea.width = (this.stage.width / this.scaleX) - 10
+	this.visibleArea.height = (this.stage.height / this.scaleY) - 10
 }
 Studio.Camera.prototype.update = function(stage, ratio, webgl) {
 	if (this.tracking) { // are we following a DisplayObject?
-		if(!webgl) this.tracking._delta(ratio)
+		if (!webgl) this.tracking._delta(ratio)
 		this.x = (this.tracking._dx)
 		this.y = (this.tracking._dy)
 	}
@@ -51,25 +56,25 @@ Studio.Camera.prototype.update = function(stage, ratio, webgl) {
 }
 
 Studio.Camera.prototype.render = function(stage, ratio, webgl) {
-	this.update(stage,ratio, webgl)
+	this.update(stage, ratio, webgl)
 	// if (this.x || this.y || this.scaleX !== this.matrix[0] || this.scaleY !== this.matrix[4]) { // we only need to update this if its different
-		this.matrix[0] = this.scaleX;
-		this.matrix[4] = this.scaleY;
-		this.matrix[6] = this.focus.x-(this.x * this.scaleX);
-		this.matrix[7] = this.focus.y-(this.y * this.scaleY);
-		if(!webgl){
-			stage.ctx.setTransform(this.matrix[0]*stage.resolution, 0, 0, this.matrix[4]*stage.resolution, this.matrix[6]*stage.resolution, this.matrix[7]*stage.resolution)
-		}
+	this.matrix[0] = this.scaleX
+	this.matrix[4] = this.scaleY
+	this.matrix[6] = this.focus.x - (this.x * this.scaleX)
+	this.matrix[7] = this.focus.y - (this.y * this.scaleY)
+	if (!webgl) {
+		stage.ctx.setTransform(this.matrix[0] * stage.resolution, 0, 0, this.matrix[4] * stage.resolution, this.matrix[6] * stage.resolution, this.matrix[7] * stage.resolution)
+	}
 	// }
-	if(webgl){ // webgl needs up to send this information.
-		stage.ctx.uniformMatrix3fv(stage.ctx.matrixLocation,false,this.matrix);
+	if (webgl) { // webgl needs up to send this information.
+		stage.ctx.uniformMatrix3fv(stage.ctx.matrixLocation, false, this.matrix)
 	}
 }
 
 Studio.Camera.prototype.track = function(who) {
 	this.tracking = who
-	this.focus.x = stage.width/2
-	this.focus.y = stage.height/2
+	this.focus.x = stage.width / 2
+	this.focus.y = stage.height / 2
 }
 
 Studio.Camera.prototype.bindTo = function(who) {
@@ -84,6 +89,6 @@ Studio.Camera.prototype.stopTracking = function() {
 	this.track(null)
 	this.focus.x = 0
 	this.focus.y = 0
-	this.matrix[6] = this.focus.x-(this.x * this.scaleX);
-	this.matrix[7] = this.focus.y-(this.y * this.scaleY);
+	this.matrix[6] = this.focus.x - (this.x * this.scaleX);
+	this.matrix[7] = this.focus.y - (this.y * this.scaleY);
 }
